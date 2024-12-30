de1ae348703f489d200b420fb8ef925de7293123
ayush <ayushmaurya2050@gmail.com>
2024-12-31 00:05:16 +0530

    NEW: setup logger and response

--- server/index.ts
+++ server/index.ts
@@ -1,11 +1,24 @@
 import express from 'express'
+import { loggerMiddleware } from './src/middlewares/logger.middleware';
+import { asyncHandler, errorHandler, errorResponse, successResponse } from './src/lib/utils';
 
 const app = express();
 
-app.get('/', (req: any, res: any) => {
-  return res.send("ok");
-})
+app.use(express.json())
+app.use(loggerMiddleware)
+
+app.get('/', asyncHandler((req: any, res: any) => {
+  return res.status(200).json(successResponse(200, { message: "Server running" }, "Request successful"))
+}))
+
+// all routes
+
+app.use((req, res, next) => {
+  res.status(404).json(errorResponse(404, "Not Found"));
+});
+  
+app.use(errorHandler);
 
 app.listen(7000, () => {
-  console.log('The server is running locally on 7000 port: http://localhost:7000');
+  console.log('The server is running locally on 7000 port: http://localhost:7000');
 })
---
+++ server/src/lib/utils.ts
@@ -0,0 +1,38 @@
+import { logger } from "./logger";
+
+export const asyncHandler = (fn: any) => async (req: any, res: any, next: any) => {
+  try {
+    await fn(req, res, next);
+  } catch (error: any) {
+    console.log(error);
+    return res.status(500).json(errorResponse(500, error.message || "An error occurred"));
+  }
+};
+
+export const successResponse = (statusCode: number, data: any, message = "Request successful") => {
+  logger.info(`RESPONSE: ${statusCode} ${message}`);
+  // logger.success(`RESPONSE: ${statusCode} ${message}`);
+  return {
+    statusCode,
+    success: true,
+    message,
+    data,
+  };
+};
+
+export const errorResponse = (statusCode: number, message = "An error occurred", errors = []) => {
+  logger.error(`ERROR: ${statusCode} ${message}`);
+  return {
+    statusCode,
+    success: false,
+    message,
+    errors,
+  };
+};
+
+export const errorHandler = (err: any, req: any, res: any, next: any) => {
+  // Optionally send error to an external monitoring service
+  // e.g., logErrorToMonitoringService(err);
+
+  res.status(500).json(errorResponse(500, "Internal Server Error"));
+};
